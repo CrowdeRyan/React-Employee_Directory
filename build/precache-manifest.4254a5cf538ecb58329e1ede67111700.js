23a400c1828f7388ce8dd59e7c03e1ae3ad45d9d
Ryan Crowder <crowderyan@gmail.com>
2021-09-13 07:56:05 -0400

    added build

---
+++ build/precache-manifest.4254a5cf538ecb58329e1ede67111700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f6ef8161290aaade45aadbe5132972",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46510f0ace6a82198566",
+    "url": "/static/css/main.6260f050.chunk.css"
+  },
+  {
+    "revision": "60c0199c40246c184583",
+    "url": "/static/js/2.0c1f134e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0c1f134e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46510f0ace6a82198566",
+    "url": "/static/js/main.f9d823ea.chunk.js"
+  },
+  {
+    "revision": "5b446e10af4fafd5a747",
+    "url": "/static/js/runtime-main.f71b05c2.js"
+  }
+]);
